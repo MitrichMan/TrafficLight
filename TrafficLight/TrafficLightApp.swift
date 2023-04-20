dbe2b5c85e4a8c3ea0059f6060a19155c06bc4cc
MitrichMan <122974224+MitrichMan@users.noreply.github.com>
2023-04-20 21:45:08 +0800

    Initial Commit

---
+++ TrafficLight/TrafficLightApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightApp.swift
+//  TrafficLight
+//
+//  Created by Dmitrii Melnikov on 20.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
